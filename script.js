33e96bdfd27ff38f91a5b9ac303a836f8f5cd5ed
SAM <75918417+SAM-dev-pixel@users.noreply.github.com>
2022-03-04 20:43:45 +0700

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-const width = document.body.clientWidth;
-document.body.style.width = (width > 800) ? '1440px' : '';
-/*fetch('https://api.shrtco.de/v2/shorten?url=https://github.com/SAM-dev-pixel?tab=repositories')
-.then(res => res.json())
-.then(shorts => console.log(shorts));*/
